64abf7b16597ad337f9044e658a4c9555ea28ad9
khel2002 <micahelroa121@gmail.com>
2025-12-01 17:19:37 +0800

    wow

--- resources/views/admin/organizations/create-organization.blade.php
+++ resources/views/admin/organizations/create-organization.blade.php
@@ -34,7 +34,7 @@
                       <option value="">Select Adviser</option>
                       @foreach($advisers as $adviser)
                           <option value="{{ $adviser->user_id }}">
-                              {{ $adviser->profile->first_name }} {{ $adviser->profile->last_name }}
+                              {{ $adviser->profile->first_name ?? " " }} {{ $adviser->profile->last_name ?? " "}}
                           </option>
                       @endforeach
                   </select>
